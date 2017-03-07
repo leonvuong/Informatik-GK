3e461263d2bbaf086801a8b9df96f91ef3787f0e
leon.vuong <leon.vuong@r210-214.schule.local>
2017-03-07 09:10:05 +0100

    Goldbachsche Verumutung Fertig LV

--- Zusatzaufgabe1/src/GoldbachscheVermutung.java
+++ Zusatzaufgabe1/src/GoldbachscheVermutung.java
@@ -7,23 +7,27 @@ public class GoldbachscheVermutung {
 		primzahlenBekommen();
 		primzahlenInList();
 		geradeZahl();
+		System.out.println("Primzahlen " + primzahlenList);
+		System.out.println("Gerade Zahlen " + geradeZahlenList);
+		addition();
 	}
 
 	int primzahl1;
 	int primzahl2;
 	int geradeZahl;
-	static int obereSchranke = 100;
+	static int obereSchranke = 100000;
 	static List<Integer> geradeZahlenList = new ArrayList<Integer>();
 	static List<Integer> primzahlenList = new ArrayList<Integer>();
-	static Primzahlsieb prim = new Primzahlsieb(100);
+	static Primzahlsieb prim = new Primzahlsieb(obereSchranke);
 	
-	public static void primzahlenBekommen()
+	
+	public static void primzahlenBekommen() //Berechnung von Primzahlen.
 	{
 		prim.primzahlenBerechnen();
 		
 	}
 	
-	public static void primzahlenInList()
+	public static void primzahlenInList() //Einsetzen der Primzahlen in eine List.
 	{
 		for(int j=0; j< prim.sieb.length;j++)
 		{
@@ -34,7 +38,7 @@ public class GoldbachscheVermutung {
 		}
 	}
 	
-	public static void geradeZahl()
+	public static void geradeZahl() //Berechnung von allen geraden Zahlen bis zur oberen Schranke.
 	{
 		for(int i=2; i < obereSchranke; i= i + 2)
 		{
@@ -43,4 +47,28 @@ public class GoldbachscheVermutung {
 		
 	}
 	
+	public static void addition() //Addition von zwei Primzahlen zu einer geraden Zahl.
+	{
+		boolean abbruch = false;
+		for(int k=0; k< geradeZahlenList.size(); k++)
+		{
+			for(int l=0; l< primzahlenList.size(); l++)
+			{
+				for(int m=0; m< primzahlenList.size(); m++)
+				{
+					if(primzahlenList.get(l) + primzahlenList.get(m) == geradeZahlenList.get(k))
+					{
+						System.out.println(primzahlenList.get(l) + "+" + primzahlenList.get(m) + "=" + geradeZahlenList.get(k));
+						abbruch=true;
+						break;
+					}
+				}
+			if(abbruch)
+			{
+				abbruch = false;
+				break;
+			}
+			}
+		}
+	}
 }
